5d6c3811867ce1fd4bf5e1ded573204e31508764
StatXzy7 <ziyangxu0205@gmail.com>
2023-10-03 17:58:17 +0000

    download update

--- data/download.sh
+++ data/download.sh
@@ -13,3 +13,8 @@ tar -xvf ./GSE144240_RAW.tar -C GSE144240_RAW/
 cd GSE144240_RAW  
 for file in *.gz; do gunzip -f "$file"; done
 
+# Get model
+cd model
+wget "https://public.sn.files.1drv.com/y4mKKprWN7l5sTKkLF3lwV3Tj1KoCWbx2VUw0cnic-0GXWzLmlVgzMBNExT7LB8490JmLRtY5sAu1B9K_LgIrU1Ft_kvbbCfwmV_9_K0DMoFaItx9f8beZIHUv6UQmHBV9Qp6q3_gWn9--8vjCj5qVgxjc7tjcdLMmg1aHQTf_VzqHmfAtREtITVHjr7aVKZ409-jSO4oxT1IuKjkkTUbuxY7t8rIRjOs5BKDsMZBi5X7A?AVOverride=1" -O ICL2ST_cSCC.pt
+wget "https://public.sn.files.1drv.com/y4mY2KthEUiV8HxqupGcVTA8fXK0fvbk3ofmAh-ayiS-9CheV5RDALka-9RMb_rVth0b7_K7_y8OFlEwXGKuNrtM0MvOQaxt17rAga2RPPa5ZxW7IXHW8WSZy0pig9oyNvJbpu89rUeLSFxE99bdHRbMQNe4UHPle3ZXkdAoY2lLhF2O25O4nCENL9pBHMX1z0ggAIN89Oh4tA4sHeVk6acJnf4bfSnuB_g49-fU7PZFKk?AVOverride=1" -O ICL2ST_HER2.pt
+
